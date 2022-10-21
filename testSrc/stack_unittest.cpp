b50adeabf10d7e87ae48e678311b68675a3f04cc
Your Name <you@example.com>
2022-10-21 17:00:53 +0800

    添加stack类的套件测试用例TEST_F(StakcTest, tsetAll)

--- testSrc/stack_unittest.cpp
+++ testSrc/stack_unittest.cpp
@@ -52,3 +52,45 @@ TEST(testStack, testAll_string)
     EXPECT_EQ("9", to_string(val).c_str());
     EXPECT_FLOAT_EQ(7.0, ((float)val)/4);
 }
+
+class StackTest:public testing:: Test{
+protected:
+    void SetUp() override
+    {
+        st.push(34);
+        st.push(28);
+        st.push(56);
+    }
+    void TearDown() override
+    {}
+    StackTest()
+    {
+        cout<<"stack is constructed."<<endl;
+        st.push(22);
+    }
+    ~StackTest()
+    {
+        cout<<"Destructed StackTest."<<endl;
+    }
+
+    stack<int> st;
+};
+
+TEST_F(StackTest, testPop)
+{
+    //自动构造一个Stack实例，并调用SetUp()函数
+    int val = st.top();
+    st.pop();
+    EXPECT_EQ(56, val);
+    //调用TearDown（）函数
+
+    st.pop();
+    st.pop();
+    int val1 = st.top();
+    EXPECT_EQ(22, val1);
+}
+TEST_F(StackTest, testPop2)
+{
+    int val = st.top();
+    EXPECT_EQ(56, val);
+}
